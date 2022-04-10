12674041d3946e0afaddbc90beab484665c054c2
Thibaut PICART <thibaut.p@outlook.com>
2022-04-10 19:50:45 +0200

    Add a menu list

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,7 +1,39 @@
 <template>
   <v-app>
     <v-main>
-      <router-view/>
+      <v-card>
+        <v-layout>
+          <v-navigation-drawer
+            class="bg-deep-purple"
+            theme="dark"
+            permanent
+          >
+            <v-list color="transparent">
+              <v-list-item>
+                <v-btn  to="/" variant="plain">
+                  <v-icon>mdi-home</v-icon>
+                  Home
+                </v-btn>
+              </v-list-item>
+              <v-list-item>
+                <v-btn  to="/groups" variant="plain">
+                  <v-icon>mdi-account-multiple</v-icon>
+                  Groups
+                </v-btn>
+              </v-list-item>
+              <v-list-item >
+                <v-btn  to="/about" variant="plain">
+                  <v-icon>mdi-help-circle</v-icon>
+                  About
+                </v-btn>
+              </v-list-item>
+            </v-list>
+          </v-navigation-drawer>
+          <v-main style="height: 100vw">
+            <router-view/>
+          </v-main>
+        </v-layout>
+      </v-card>
     </v-main>
   </v-app>
 </template>
