d8bff0f2716ed188c64db71468c45cc6cb18a02d
Thibaut PICART <thibaut.p@outlook.com>
2022-04-10 19:52:34 +0200

    Add a fetch to get the api data

--- admin/src/views/GroupsView.vue
+++ admin/src/views/GroupsView.vue
@@ -1,38 +1,50 @@
 <template>
-    <v-expansion-panels>
-        <v-expansion-panel
-            v-for="group in groups"
-            :key="group.group_id"
-        >
-            <v-expansion-panel-title>
-                <v-btn @click="renameGroup($event, group.group_id)" icon=mdi-pencil class="mr-4"></v-btn>
-                {{ group.group_name }}
-                <v-text-field
-                    label="Nouveau nom" class="d-none group-{{group.group_id}}"
-                ></v-text-field>
-            </v-expansion-panel-title>
-            <v-expansion-panel-text>
-                <v-list
-                    v-for="(user,i) in group.users_names"
-                    :key="i"
-                >
-                    <v-list-item>
-                        <v-list-item-header>
-                            {{ user.users_names }}
-                        </v-list-item-header>
-                        <template v-slot:append><v-btn icon=mdi-account-multiple-minus></v-btn></template>
-                    </v-list-item>
-                    
-                </v-list>
-            </v-expansion-panel-text>
-        </v-expansion-panel>
-    </v-expansion-panels>
+    <v-container>
+        <v-expansion-panels>
+            <v-expansion-panel
+                v-for="group in groups"
+                :key="group.group_id"
+            >
+                <v-expansion-panel-title>
+                    {{ group.group_name }}
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                    <v-list
+                        v-for="(user,i) in group.users_names"
+                        :key="i"
+                    >
+                        <v-list-item
+                            density='compact'
+                        >
+                            <v-list-item-header>
+                                {{ user.users_names }}
+                            </v-list-item-header>
+                        </v-list-item>
+                    </v-list>
+                </v-expansion-panel-text>
+            </v-expansion-panel>
+        </v-expansion-panels>
+    </v-container>
 </template>
 
 <script>
-fetch("0.0.0.0:5000/api/v1/students", {
-    "method": "GET"
+const fetch_groups = fetch("https://0.0.0.0:5000/api/v1/students", {
+    "method": "GET",
 })
+.then(response => {
+    if (response.ok) {
+        return response.json();
+    } else {
+        console.log(response.statusText);
+    }
+})
+.then(json => {
+    return json.body;
+})
+.catch(err=>{
+    console.log(err);
+});
+console.log(fetch_groups);
 export default {
     data () {
         return {
@@ -64,17 +76,5 @@ export default {
             ],
         }
     },
-    methods: {
-        deleteUser: function () {
-            fetch("0.0.0.0:5000/", {
-                "method": ""
-            })
-        },
-        renameGroup: function(event, group_id) {
-            const input = document.getElementsByClassName('group-'+group_id);
-            console.log(input);
-            input[0].style.display = "block";
-        }
-    }
 }
 </script>
