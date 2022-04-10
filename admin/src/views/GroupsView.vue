10fae95fc7446d21846b23af9837d0650aa123b9
Thibaut PICART <thibaut.p@outlook.com>
2022-04-10 19:52:34 +0200

    Add a fetch to get the api data

--- admin/src/views/GroupsView.vue
+++ admin/src/views/GroupsView.vue
@@ -1,32 +1,50 @@
 <template>
-    <v-expansion-panels>
-        <v-expansion-panel
-            v-for="group in groups"
-            :key="group.group_id"
-        >
-            <v-expansion-panel-title>
-                {{ group.group_name }}
-            </v-expansion-panel-title>
-            <v-expansion-panel-text>
-                <v-list
-                    v-for="(user,i) in group.users_names"
-                    :key="i"
-                >
-                    <v-list-item>
-                        <v-list-item-header>
-                            {{ user.users_names }}
-                        </v-list-item-header>
-                    </v-list-item>
-                </v-list>
-            </v-expansion-panel-text>
-        </v-expansion-panel>
-    </v-expansion-panels>
+    <v-container>
+        <v-expansion-panels>
+            <v-expansion-panel
+                v-for="group in groups"
+                :key="group.group_id"
+            >
+                <v-expansion-panel-title>
+                    {{ group.group_name }}
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                    <v-list
+                        v-for="(user,i) in group.users_names"
+                        :key="i"
+                    >
+                        <v-list-item
+                            density='compact'
+                        >
+                            <v-list-item-header>
+                                {{ user.users_names }}
+                            </v-list-item-header>
+                        </v-list-item>
+                    </v-list>
+                </v-expansion-panel-text>
+            </v-expansion-panel>
+        </v-expansion-panels>
+    </v-container>
 </template>
 
 <script>
-fetch("0.0.0.0:5000/api/v1/students", {
-    "method": "GET"
+const fetch_groups = fetch("https://0.0.0.0:5000/api/v1/students", {
+    "method": "GET",
 })
+.then(response => {
+    if (response.ok) {
+        return response.json();
+    } else {
+        console.log(response.statusText);
+    }
+})
+.then(json => {
+    return json.body;
+})
+.catch(err=>{
+    console.log(err);
+});
+console.log(fetch_groups);
 export default {
     data () {
         return {
