881f22c376992a16322d47c565c639f9cc0af318
Thibaut PICART <thibaut.p@outlook.com>
2022-04-10 18:24:17 +0200

    Add front for groups list

--- admin/src/router/index.ts
+++ admin/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import GroupsView from "../views/GroupsView.vue";
+import GroupsEdit from "../views/GroupsEdit.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,9 +20,14 @@ const router = createRouter({
       component: () => import("../views/AboutView.vue"),
     },
     {
-      path: "/edit-group",
-      name: "edit-group",
-      component: () => import("../views/EditGroup.vue"),
+      path: "/groups",
+      name: "groups",
+      component: GroupsView,
+    },
+    {
+      path: "/groupsEdit/:id_group",
+      name: "groupsEdit",
+      component: GroupsEdit,
     },
   ],
 });
