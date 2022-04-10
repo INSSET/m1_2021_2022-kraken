b57c1065a37cd6890961649eebb79886c3cf92a2
Midoclas <tanguy.paquet02@gmail.com>
2022-04-10 15:40:58 +0200

    Vuetify installation

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -4,8 +4,10 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import store from './plugins/vuex';
 import { loadFonts } from './plugins/webfontloader'
+import { createVuetify } from 'vuetify/lib/framework'
 
 loadFonts()
+const vuetify = createVuetify()
 
 createApp(App)
   .use(router)
