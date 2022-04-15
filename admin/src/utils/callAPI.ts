1a0ee344700033373033a1b14555eb806fce69ab
Thibaut PICART <thibaut.p@outlook.com>
2022-04-15 10:37:15 +0200

    add specific files to request the API

---
+++ admin/src/utils/callAPI.ts
@@ -0,0 +1,15 @@
+const BASE_URL = "https://0.0.0.0:5000/api/v1/";
+
+function fetchAPI<T>(sub_URL: string, method: string): Promise<T> {
+    return fetch(BASE_URL + sub_URL, {
+        "method": method,
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        return response.json();
+    });
+}
+
+export default fetchAPI;
