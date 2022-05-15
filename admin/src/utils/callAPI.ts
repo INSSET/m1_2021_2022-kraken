00d83cdd9e3fc67fd5da2478a1083e2af0c1106d
Thibaut PICART <thibaut.p@outlook.com>
2022-05-15 17:09:42 +0200

    modification for callAPI

--- admin/src/utils/callAPI.ts
+++ admin/src/utils/callAPI.ts
@@ -1,8 +1,9 @@
 const BASE_URL = "http://0.0.0.0:5000/api/v1/";
 
-function fetchAPI<T>(sub_URL: string, method: string): Promise<T> {
+function fetchAPI<T>(sub_URL: string, method: string, body : object): Promise<T> {
     return fetch(BASE_URL + sub_URL, {
         "method": method,
+        body: JSON.stringify(body),
     })
     .then(response => {
         if (!response.ok) {
--- admin/src/utils/requests.ts
+++ admin/src/utils/requests.ts
@@ -3,22 +3,21 @@ import fetchAPI from "./callAPI";
 
 enum Method {
     GET = "GET",
+    POST = "POST",
 }
 enum APIRoutes {
     getGroups = "groups",
 }
 
 export function getGroups() {
-    fetchAPI(APIRoutes.getGroups, Method.GET)
+    fetchAPI(APIRoutes.getGroups, Method.GET, {})
     .then(response => {
-        console.log(response);
         store.commit({
             type: "setGroups",
             groups: response,
         })
     })
     .catch(err => {
-        console.log(err);
         store.commit({
             type: "setGroups",
             groups: [],
