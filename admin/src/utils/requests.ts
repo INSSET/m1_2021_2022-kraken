27b1625b72ec3532c7ace4d2aa4ac1ccdc7132d4
Thibaut PICART <thibaut.p@outlook.com>
2022-05-15 17:09:42 +0200

    modification for callAPI

--- admin/src/utils/requests.ts
+++ admin/src/utils/requests.ts
@@ -3,75 +3,24 @@ import fetchAPI from "./callAPI";
 
 enum Method {
     GET = "GET",
+    POST = "POST",
 }
 enum APIRoutes {
-    getGroups = "students",
+    getGroups = "groups",
 }
 
 export function getGroups() {
-    fetchAPI(APIRoutes.getGroups, Method.GET)
+    fetchAPI(APIRoutes.getGroups, Method.GET, {})
     .then(response => {
-        console.log(response);
         store.commit({
             type: "setGroups",
-            groups: [
-                {
-                    group_id: 1,
-                    group_name: "Group 1",
-                    users_names: [
-                        "toto",
-                        "tata",
-                    ],
-                },
-                {
-                    group_id: 2,
-                    group_name: "Group 2",
-                    users_names: [
-                        "tutu",
-                        "titi",
-                    ],
-                },
-                {
-                    group_id: 3,
-                    group_name: "Group 3",
-                    users_names: [
-                        "tita",
-                        "titutu",
-                    ],
-                },
-            ],
+            groups: response,
         })
     })
     .catch(err => {
-        console.log(err);
         store.commit({
             type: "setGroups",
-            groups: [
-                {
-                    group_id: 1,
-                    group_name: "Group 1",
-                    users_names: [
-                        "toto",
-                        "tata",
-                    ],
-                },
-                {
-                    group_id: 2,
-                    group_name: "Group 2",
-                    users_names: [
-                        "tutu",
-                        "titi",
-                    ],
-                },
-                {
-                    group_id: 3,
-                    group_name: "Group 3",
-                    users_names: [
-                        "tita",
-                        "titutu",
-                    ],
-                },
-            ],
+            groups: [],
         })
     })
 }
