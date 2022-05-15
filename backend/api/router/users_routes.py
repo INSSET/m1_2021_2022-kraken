f8f6e18427e3a377a4c5a8ce097e0b525b4b62b3
MonsieurMickeNiepceron <92097834+MonsieurMickeNiepceron@users.noreply.github.com>
2022-05-15 10:46:01 +0200

    Add delete user and add new user + documentation

--- backend/api/router/users_routes.py
+++ backend/api/router/users_routes.py
@@ -1,21 +1,20 @@
 import json
 import os
-import pathlib
 import pwd
-
-from flask import Blueprint, Response, abort, request
 import sys
 
+from flask import Blueprint, Response, abort, request
+
+import gestprojlib as GP
 from api.model.user import User
 from api.utils.json_encoder import Encoder
-import gestprojlib as GP
 
 sys.path.append("/usr/src/app/gplib")
 
 users_routes = Blueprint("users_routes", __name__)
 
 
-@users_routes.route("/api/users", methods=["OPTIONS"])
+@users_routes.route("/api/v1/students", methods=["OPTIONS"])
 def preflight_get_users():
     """ Le front en VueJS execute une requête préliminaire pour vérifier la possibilité de faire un GET
     par défaut flask renvoie une redirection 302 qui n'est pas pris en charge par les navigateurs
@@ -23,7 +22,7 @@ def preflight_get_users():
     return Response(response=None, status=204)
 
 
-@users_routes.route("/api/users/", methods=["GET"])
+@users_routes.route("/api/v1/students/", methods=["GET"])
 def get_users():
     """ Retourne l'id, le nom et l'id du group des users dont l'id est supérieur à 10000 """
     users = []
@@ -40,7 +39,7 @@ def get_users():
     return Response(encoded_users, mimetype="application/json", status=200)
 
 
-@users_routes.route("/api/users/<int:user_id>/", methods=["GET"])
+@users_routes.route("/api/v1/students/<int:user_id>/", methods=["GET"])
 def get_user_by_id(user_id):
     try:
         user_trouve = pwd.getpwuid(user_id)
@@ -60,7 +59,7 @@ def get_user_by_id(user_id):
         abort(404, description="Resources not found")
 
 
-@users_routes.route("/api/create-users/", methods=["POST"])
+@users_routes.route("/api/v1/students/", methods=["POST"])
 def create_user():
     try:
         request_data = request.get_json()
@@ -69,40 +68,57 @@ def create_user():
         user_name = user_firstname + "." + user_lastname
         if pwd.getpwnam(user_name):
             response = {
-                "message": "L\'étudiant·e existe déjà !",
-                "icon": "mdi-alert-decagram",
-                "color": "error"
+                "message": "L\'étudiant·e existe déjà !"
             }
             return Response(json.dumps(response), mimetype="application/json", status=409)
     except KeyError:
         request_data = request.get_json()
         user_firstname = request_data["firstname"]
         user_lastname = request_data["lastname"]
-        user_group_id = int(request_data["groupId"])
+        user_group_name = request_data["groupName"]
         shell = "/bin/bash"
         skelpath = "../conf/skel"
         user_name = user_firstname + "." + user_lastname
-        path = pathlib.Path().absolute()
-        student_dir_path = str(path) + "/../home/etudiants/" + user_name
+        student_dir_path = "/home/etudiants/" + user_name
         os.makedirs(student_dir_path)
 
         os.system("useradd -d /home/etudiants/%s -K UID_MIN=10002 -m --skel %s -g %s -N --shell %s %s" % (
-            user_name, skelpath, user_group_id, shell, user_name))
+            user_name, skelpath, user_group_name, shell, user_name))
         os.system("usermod -p '*' %s" % user_name)
-        os.system("usermod -a -G %s %s" % (user_group_id, user_name))
+        os.system("usermod -a -G %s %s" % (user_group_name, user_name))
         os.system("chown root:root /home/etudiants/%s" % user_name)
 
-        user = pwd.getpwnam(user_name)
-        new_user = User(user.pw_uid, user.pw_name, user.pw_gid)
-        encoded_user = json.dumps(new_user, cls=Encoder)
+        os.system("useradd -d /home/etudiants/%s/sftp -K UID_MIN=10002 --shell %s -N -g %s sftp.%s" % (
+            user_name, shell, 'sftp', user_name))
+        os.system("usermod -p '*' sftp.%s" % user_name)
+        os.system("mkdir -p /home/etudiants/%s/sftp/Projets" % user_name)
+        os.system("mkdir -p /home/etudiants/%s/sftp/.ssh" % user_name)
+        os.system("chown root:root /home/etudiants/%s/sftp" % user_name)
+        os.system("chown sftp.%s:sftp /home/etudiants/%s/sftp/Projets" % (user_name, user_name))
+        os.system("chown sftp.%s:sftp /home/etudiants/%s/sftp/.ssh" % (user_name, user_name))
+        os.system("chmod 775 /home/etudiants/%s/sftp/Projets" % user_name)
 
-        return Response(encoded_user, mimetype="application/json", status=201)
+        return Response(mimetype="application/json", status=201)
 
 
-@users_routes.route("/api/users/group/<string:group_name>/", methods=["GET"])
+@users_routes.route("/api/v1/students/group/<string:group_name>/", methods=["GET"])
 def get_users_by_group(group_name):
     """
     Renvoie la liste de tous les utilisateurs appartenant au group "group_name"
     """
     liste = GP.liste_etudiant_group(group_name)
     return Response(json.dumps(liste, cls=Encoder), mimetype="application/json", status=200)
+
+
+@users_routes.route('/api/v1/students/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    try:
+        user = pwd.getpwuid(user_id)
+
+        os.system('userdel -f --remove %s' % user.pw_name)
+        os.system("userdel -f --remove sftp.%s" % user.pw_name)
+
+        return Response('User ' + user.pw_name + ' has been deleted', mimetype='application/json', status=200)
+    except KeyError:
+        abort(404, description='Not found - Could not find user with ID ' + user_id)
+
