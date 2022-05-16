ccb1929f5e9ac8b20d5fdb0374055b0ec70ed5c2
Midoclas <tanguy.paquet02@gmail.com>
2022-05-16 10:02:16 +0200

    fix conflict

--- admin/src/router/index.ts
+++ admin/src/router/index.ts
@@ -3,7 +3,6 @@ import HomeView from "../views/HomeView.vue";
 import GroupsView from "../views/GroupsView.vue";
 import AddGroupView from "../views/AddGroupView.vue";
 import GroupsEdit from "../views/GroupsEdit.vue";
-import GroupDetailView from "../views/GroupDetailView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
