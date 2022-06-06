5c613f143f90124d8263aac93c885d63d341eedd
Thibaut PICART <thibaut.p@outlook.com>
2022-06-06 17:46:58 +0200

    modification AddGroupView

--- admin/src/views/AddGroupView.vue
+++ admin/src/views/AddGroupView.vue
@@ -1,44 +1,54 @@
 <template>
     <v-container>
-        <v-form
-
-        >
+        <v-form>
             <v-text-field
                 label="Nom du groupe"
                 v-model="group_name"
+                class="my-4"
             ></v-text-field>
             <v-file-input
                 accept=".csv"
                 label="Liste des étudiants"
                 @change="onChangeFile"
+                class="my-4"
             ></v-file-input>
-            <v-table
-                :class="{'d-none': (students_names.length === 0)}"
-                class="mx-4 mb-4"
-            >
-                <thead>
-                    <tr>
-                        <th class="text-left">
-                            Nom
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr
-                        v-for="student_name in students_names"
-                        :key="student_name"
-                    >
-                        <td>{{ student_name }}</td>
-                    </tr>
-                </tbody>
-            </v-table>
             <v-btn
-                class="bg-deep-purple"
+                class="bg-deep-purple mb-4"
                 @click="onSubmit"
             >
                 Créer le groupe
             </v-btn>
         </v-form>
+        
+        <v-expansion-panels>
+            <v-expansion-panel>
+                <v-expansion-panel-title>
+                    Contenue du fichier
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                    <v-table
+                        :class="{'d-none': (students_names.length === 0)}"
+                        class="mx-4 mb-4"
+                    >
+                        <thead>
+                            <tr>
+                                <th class="text-left">
+                                    Nom
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                                v-for="student_name in students_names"
+                                :key="student_name"
+                            >
+                                <td>{{ student_name }}</td>
+                            </tr>
+                        </tbody>
+                    </v-table>
+                </v-expansion-panel-text>
+            </v-expansion-panel>
+        </v-expansion-panels>
     </v-container>
 </template>
 
