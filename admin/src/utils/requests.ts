c4a5dcbed5d1f633bc25af31ff5643d3052842e6
Thibaut PICART <thibaut.p@outlook.com>
2022-06-11 18:36:44 +0200

    refacto call api getGroups

--- admin/src/utils/requests.ts
+++ admin/src/utils/requests.ts
@@ -1,48 +1,46 @@
 import store from "@/plugins/vuex";
 import fetchAPI from "./callAPI";
 import axios from "axios";
+import Group from "@/models/group";
+import Student from "@/models/student";
 
 const BASE_URL = "http://backend.insset.localhost/api/v1/";
 
-enum Method {
-    GET = "GET",
-    POST = "POST",
-}
 enum APIRoutes {
-    getGroups = "groups",
+    groups = "groups",
 }
 
 export function getGroups() {
-    fetchAPI(APIRoutes.getGroups, Method.GET, {})
-    .then(response => {
-        store.commit({
-            type: "setGroups",
-            groups: response,
-        })
-    })
-    .catch(err => {
-        store.commit({
-            type: "setGroups",
-            groups: [],
-        })
-    })
-    axios.get(`${BASE_URL}/${APIRoutes.getGroups}`)
+    axios.get(`${BASE_URL}${APIRoutes.groups}`)
         .then(res => {
-            console.log(`retour axios :`)
-            console.log(res)
+
+            // Transformaion des données
+            let groups: Array<Group> = [];
+            res.data.forEach((group: { group_id: number, group_name: string, users_names: string[]; }) => {
+                let students: Array<Student> = [];
+                group.users_names.forEach(studentName => {
+                    students.push(new Student(studentName))
+                });
+                groups.push(new Group(group.group_id, group.group_name, students))
+            });
+
+            // Mise à jour du store 
+            store.commit({
+                type: "setGroups",
+                groups: groups,
+            })
+
         })
 }
 
 export function createGroupsWithCSV(group_name : string, file : object) {
 
-    axios.post(`http://backend.insset.localhost/api/v1/groups/${group_name}/upload`, {
+    axios.post(`${BASE_URL}${APIRoutes.groups}/${group_name}/upload`, {
         file: file
     }, {
         headers: {
             'Content-Type': 'multipart/form-data'
         },
-    }).then(res => {
-        console.log(res)
     })
 
 }
