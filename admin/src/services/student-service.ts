ea8adb251ecf37ab34340df73f2dd8b7d57139cf
Midoclas <tanguy.paquet02@gmail.com>
2022-06-13 16:43:31 +0200

    fix ssh service

--- admin/src/services/student-service.ts
+++ admin/src/services/student-service.ts
@@ -82,18 +82,4 @@ export default new class StudentService {
             }
         })
     }
-
-    /**
-     * 
-     * @param idStudent 
-     */
-     async addSshKey(idStudent: string) {
-        axios.post("http://0.0.0.0:5000/api/v1/students"+idStudent+"/ssh/upload").then(function (response) {
-            if (response.status == 200) {
-                container.commit({
-                    type: "reloadSshKey"
-                })
-            }
-        })
-    }
 }
